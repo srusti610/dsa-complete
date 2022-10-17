907e41ca37f0f3251395303b782b0aa4f65930db
Srusti Gourani <79088553+srusti610@users.noreply.github.com>
2022-10-17 19:36:25 +0530

    Create starting_point_of_linked_list_loop.cpp

---
+++ starting_point_of_linked_list_loop.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL ||head->next==NULL)
+            return NULL;                                           //be careful
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *entry=head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(slow!=entry)
+                {
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+            
+            return entry;                               //becareful
+            }
+        }   
+    
+        return NULL;
+    }
+        
+};
