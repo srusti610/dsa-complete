bf280d6edcf0869bd42aa128584321ab59921ad8
Srusti Gourani <79088553+srusti610@users.noreply.github.com>
2022-10-19 19:01:05 +0530

    Update max_consecutive_ones.cpp

--- max_consecutive_ones.cpp
+++ max_consecutive_ones.cpp
@@ -3,7 +3,7 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int cnt=0;
         int maxi=0;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++){
             if(nums[i]==1)
             {
                 cnt++;
@@ -14,6 +14,7 @@ public:
             
         }
         maxi=max(maxi,cnt);
+        }
             return maxi;
         
     }
